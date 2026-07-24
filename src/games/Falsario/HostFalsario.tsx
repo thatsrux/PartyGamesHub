37dbd10d0de53352e124eab13fc39f828edfb0d7
unknown <vincenzom121@gmail.com>
2026-07-24 17:03:57 +0200

    fix(ui): change falsario deception text

--- src/games/Falsario/HostFalsario.tsx
+++ src/games/Falsario/HostFalsario.tsx
@@ -185,7 +185,7 @@ export default function HostFalsario({ lobbyCode }: { lobbyCode: string }) {
                   
                   return (
                     <div key={id} style={{ fontSize: '1.2rem' }}>
-                      <span style={{ fontWeight: 'bold' }}>{p.name}</span> ha creduto alla bugia di <span style={{ color: 'var(--color-danger)', fontWeight: 'bold' }}>{liarName}</span>! <span style={{ color: 'var(--color-warning)' }}>(+100 a {liarName})</span>
+                      <span style={{ color: 'var(--color-danger)', fontWeight: 'bold' }}>{liarName}</span> ha ingannato <span style={{ fontWeight: 'bold' }}>{p.name}</span>! <span style={{ color: 'var(--color-warning)' }}>(+100)</span>
                     </div>
                   );
                 } else {
